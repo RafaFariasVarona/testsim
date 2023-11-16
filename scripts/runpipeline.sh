21898facc2d3c724fa6206d48cb3589c2940a449
Rafa Farias <rafafariasupv@gmail.com>
2023-11-16 19:40:45 +0100

    This script obtains all sample ids from data directory and runs analyse_samples.sh script. Finally generates a report with MultiQC and redirects the output from your main script to a file

--- scripts/runpipeline.sh
+++ scripts/runpipeline.sh
@@ -2,10 +2,9 @@ mkdir -p ~/testsim/res/genome
 wget -O ~/testsim/res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 gunzip ~/testsim/res/genome/ecoli.fasta.gz
 for sample in $(ls ../data/*.fastq.gz | cut -d"_" -f1 | sed "s:../data/::" | sort | uniq)
-do bash ~/testsim/scripts/analyse_samples.sh $sample
+do bash ~/testsim/scripts/analyse_samples.sh $sample &> ~/testsim/log/runpipeline.out
 done
 echo "Running MultiQC..."
     mkdir -p ~/testsim/out/multiqc
     multiqc -o ~/testsim/out/multiqc ~/testsim
     echo
-bash runpipeline.sh &> ~/testsim/log/runpipeline.out
