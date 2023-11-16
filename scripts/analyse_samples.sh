cc071b33da17038b3e82f607f88b3db0e14f171b
Rafa Farias <rafafariasupv@gmail.com>
2023-11-16 18:43:03 +0100

    Analyse samples script

--- scripts/analyse_samples.sh
+++ scripts/analyse_samples.sh
@@ -2,21 +2,21 @@ if [ "$#" -eq 1 ]
 then
     sampleid=$1
     echo "Running FastQC..."
-    mkdir -p out/fastqc
-    fastqc -o out/fastqc data/${sampleid}*.fastq.gz
+    mkdir -p ~/testsim/out/fastqc
+    fastqc -o ~/testsim/out/fastqc ~/testsim/data/${sampleid}*.fastq.gz
     echo
     echo "Running cutadapt..."
-    mkdir -p log/cutadapt
-    mkdir -p out/cutadapt
-    cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz > log/cutadapt/${sampleid}.log
+    mkdir -p ~/testsim/log/cutadapt
+    mkdir -p ~/testsim/out/cutadapt
+    cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o ~/testsim/out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p ~/testsim/out/cutadapt/${sampleid}_2.trimmed.fastq.gz ~/testsim/data/${sampleid}_1.fastq.gz ~/testsim/data/${sampleid}_2.fastq.gz > ~/testsim/log/cutadapt/${sampleid}.log
     echo
     echo "Running STAR index..."
-    mkdir -p res/genome/star_index
-    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
+    mkdir -p ~/testsim/res/genome/star_index
+    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir ~/testsim/res/genome/star_index/ --genomeFastaFiles ~/testsim/res/genome/ecoli.fasta --genomeSAindexNbases 9
     echo
     echo "Running STAR alignment..."
-    mkdir -p out/star/${sampleid}
-    STAR --runThreadN 4 --genomeDir res/genome/star_index/ --readFilesIn out/cutadapt/${sampleid}_1.trimmed.fastq.gz out/cutadapt/${sampleid}_2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix out/star/${sampleid}/
+    mkdir -p ~/testsim/out/star/${sampleid}
+    STAR --runThreadN 4 --genomeDir ~/testsim/res/genome/star_index/ --readFilesIn ~/testsim/out/cutadapt/${sampleid}_1.trimmed.fastq.gz ~/testsim/out/cutadapt/${sampleid}_2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix ~/testsim/out/star/${sampleid}/
     echo
 else
     echo "Usage: $0 <sampleid>"
