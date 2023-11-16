e8398329640a2ab24d664270627d85f24317e399
Rafa Farias <rafafariasupv@gmail.com>
2023-11-16 19:53:14 +0100

    Analyse samples script

--- scripts/analyse_samples.sh
+++ scripts/analyse_samples.sh
@@ -10,10 +10,6 @@ then
     mkdir -p ~/testsim/out/cutadapt
     cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o ~/testsim/out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p ~/testsim/out/cutadapt/${sampleid}_2.trimmed.fastq.gz ~/testsim/data/${sampleid}_1.fastq.gz ~/testsim/data/${sampleid}_2.fastq.gz > ~/testsim/log/cutadapt/${sampleid}.log
     echo
-    echo "Running STAR index..."
-    mkdir -p ~/testsim/res/genome/star_index
-    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir ~/testsim/res/genome/star_index/ --genomeFastaFiles ~/testsim/res/genome/ecoli.fasta --genomeSAindexNbases 9
-    echo
     echo "Running STAR alignment..."
     mkdir -p ~/testsim/out/star/${sampleid}
     STAR --runThreadN 4 --genomeDir ~/testsim/res/genome/star_index/ --readFilesIn ~/testsim/out/cutadapt/${sampleid}_1.trimmed.fastq.gz ~/testsim/out/cutadapt/${sampleid}_2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix ~/testsim/out/star/${sampleid}/
