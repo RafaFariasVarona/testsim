0d6216cd0e658c5bf634cac9775b68c45703db0b
Rafa Farias <rafafariasupv@gmail.com>
2023-11-16 18:35:25 +0100

    This script obtains all sample ids from data directory and runs analyse_samples.sh script

--- scripts/runpipeline.sh
+++ scripts/runpipeline.sh
@@ -1,4 +1,4 @@
 mkdir -p res/genome
 wget -O res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
 gunzip res/genome/ecoli.fasta.gz
-for sample in $(ls ../data/*.fastq.gz | cut -d"_" -f1 | sed "s:../data/::" | sort | uniq); do bash scripts/analyse_samples.sh $sample; done;
+for sample in $(ls ../data/*.fastq.gz | cut -d"_" -f1 | sed "s:../data/::" | sort | uniq); do bash ~/testsim/scripts/analyse_samples.sh $sample; done;
